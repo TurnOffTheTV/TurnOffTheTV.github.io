378d423b4689f184524d3c800b0fdfa7114f4fd0
TurnOffTheTV <pfilmsxtras@gmail.com>
2021-05-08 17:23:25 -0400

    fixed LBP logo widths

--- programs/main.js
+++ programs/main.js
@@ -3,15 +3,12 @@ var isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 if(isDark){console.log("Dark");}
 else{console.log("Light");}
 
-function tick() {
-if (document.hasFocus()){console.log("Document in focus");}
-else{console.log("Document NOT in focus");}
-}
-
-setInterval("tick()", 1);
-
 var unavaliableEls = document.getElementsByClassName("unavaliable");
 
 for (var i = 0; i < unavaliableEls.length; i++) {
                 unavaliableEls[i].innerHTML += " (<span>Unavaliable</span>)";
             }
+var nameEls = document.getElementsByClassName("game");
+
+if(game==="plat"){nameEls.innerHTML = "2D Platformer";}
+if(game==="nswitch"){nameEls.innerHTML = "Nintendo Switch";}
