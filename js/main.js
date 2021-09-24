48c1bcb82665a970a968f58e84d1c34aeb17ef51
TurnOffTheTV <66259107+TurnOffTheTV@users.noreply.github.com>
2021-09-24 08:28:45 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-//dark/light mode
-var style = document.getElementById("style");
-
-setInterval(function(){
-	var isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-	if(isDark){style.innerHTML='<link href="https://turnoffthetv.github.io/css/dark.css" rel="stylesheet" id="style">';}
-	else{style.innerHTML='<link href="https://turnoffthetv.github.io/css/light.css" rel="stylesheet" id="style">';}
-}, 10);
-
 var unavaliableEls = document.getElementsByClassName("unavaliable");
 
 for (var i = 0; i < unavaliableEls.length; i++) {
