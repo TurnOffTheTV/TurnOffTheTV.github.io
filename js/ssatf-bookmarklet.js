31f9b877bd04beb77cc27e68f846ff35859e2588
TurnOffTheTV <66259107+TurnOffTheTV@users.noreply.github.com>
2021-12-08 14:30:09 -0500

    Update ssatf-bookmarklet.js

--- js/ssatf-bookmarklet.js
+++ js/ssatf-bookmarklet.js
@@ -4,6 +4,6 @@ document.body.appendChild(sketch);
 
 function setup(){
   createCanvas(100,100);
-  canvas.parent("sketch-container");
+  canvas.parent="sketch-container";
   background(0,0,255);
 }
