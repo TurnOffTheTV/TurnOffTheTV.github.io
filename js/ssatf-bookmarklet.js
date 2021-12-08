0c47ccf62a810ee6389a0918124586dca343c859
TurnOffTheTV <66259107+TurnOffTheTV@users.noreply.github.com>
2021-12-08 14:24:31 -0500

    Update ssatf-bookmarklet.js

--- js/ssatf-bookmarklet.js
+++ js/ssatf-bookmarklet.js
@@ -1,19 +1,9 @@
-var snooty = document.createElement("div");
-var snooty_img = document.createElement("img");
-var px = 0;
-var py = 0;
-var fall = 0;
-var jump = 0;
-var onFloor = 0;
-setInterval(function(){
-  snooty.style.position="float";
-  snooty.style.left=px+"px";
-  snooty.style.top=py+"px";
-  snooty.id="snooty"
-  snooty_img.src="https://turnoffthetv.github.io/images/snooty-favicon.png";
-  snooty_img.width=500;
-  snooty_img.height=500;
-  px+=1;
-}, 100);
-document.body.appendChild(snooty);
-snooty.appendChild(snooty_img);
+var div = document.createElement("div");
+div.id="sketch-container";
+document.body.appendChild(sketch);
+
+function setup(){
+  createCanvas(100,100);
+  canvas.parent("sketch-container");
+  background(0,0,255);
+}
