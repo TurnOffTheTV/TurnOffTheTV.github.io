ec30c7eed2c7132038a4c4c998755090fa6bbd5c
TurnOffTheTV <66259107+TurnOffTheTV@users.noreply.github.com>
2022-01-18 14:19:09 -0500

    Create funmathgame.js

---
+++ js/funmathgame.js
@@ -0,0 +1,14 @@
+var level = 0;
+var problems;
+
+function preload(){
+  problems = loadJSON("problems.json");
+}
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+  background(0);
+}
