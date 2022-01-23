92e1542eaa85c8dfbee1635013c3a5eaa0c13a2b
TurnOffTheTV <pfilmsxtras@gmail.com>
2022-01-23 18:04:04 -0500

    edited

---
+++ js/3dtest.js
@@ -0,0 +1,32 @@
+var xRot = 0;
+var yRot = 0;
+var teapot;
+
+function preload(){
+	teapot = loadModel("teapot.obj");
+}
+
+function setup(){
+  createCanvas(windowWidth,windowHeight,WEBGL);
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth,windowHeight);
+}
+
+var mousePressed =function(){requestPointerLock();}
+
+function draw(){
+	xRot+=movedY/100;
+	yRot+=movedX/100;
+  background(0);
+	rotateX(xRot);
+	rotateY(yRot);
+	scale(5.596063232421875);
+	fill(255,255,255,128);
+	stroke(255);
+	sphere(150,20,20);
+	scale(15);
+	fill(100);
+	model(teapot);
+}
