930c8eb3b0a2dc4b77a2dce0a9feb58ef3b67052
TurnOffTheTV <pfilmsxtras@gmail.com>
2022-02-08 07:47:02 -0500

    Update 3dtest.js

--- js/3dtest.js
+++ js/3dtest.js
@@ -6,6 +6,7 @@ var cy = 0;
 var cz = 0;
 var controlMode = 0;
 var teapot;
+var keys = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
 
 
 function preload(){
@@ -14,6 +15,7 @@ function preload(){
 
 function setup(){
   createCanvas(windowWidth,windowHeight,WEBGL);
+	noStroke();
 	//debugMode();
 }
 
@@ -25,30 +27,55 @@ function mouseMoved(){
 	controlMode=0;
 }
 
-var mousePressed =function(){requestPointerLock();fullscreen(true);}
+var keyPressed =function(){controlMode===0;keys[keyCode]=true;}
+
+var keyReleased =function(){keys[keyCode]=false;}
+
+function mousePressed(){requestPointerLock();fullscreen(true);}
 
 function draw(){
-	noStroke();
-	ambientLight(100);
 	if(controlMode===0){
 		cx+=movedY;
 		cy+=movedX;
+		if(keys[87]){px+=5*-sin(radians(-cy));pz+=5*-cos(radians(-cy));}
+		if(keys[83]){px+=5*sin(radians(-cy));pz+=5*cos(radians(-cy));}
+		if(keys[65]){px+=5*sin(radians(cy));pz+=5*cos(radians(cy));}
+		if(keys[68]){px+=5*-sin(radians(cy));pz+=5*-cos(radians(-cy));}
 	}
 	if(controlMode===1){
 		if(p1.stick.ry>0.2 || p1.stick.ry<-0.2){cx+=3*p1.stick.ry;}
 		if(p1.stick.rx>0.2 || p1.stick.rx<-0.2){cy+=3*p1.stick.rx;}
+		if(p1.stick.ly<-0.2){px+=abs(5*p1.stick.ly)*-sin(radians(-cy));pz+=5*-cos(radians(-cy));}
+		if(p1.stick.ly>0.2){px+=abs(5*p1.stick.ly)*sin(radians(-cy));pz+=5*cos(radians(-cy));}
+		if(keys[65]){px+=5*sin(radians(-cy));pz+=5*-cos(radians(cy));}
+		if(keys[68]){px+=5*-sin(radians(cy));pz+=5*cos(radians(-cy));}
 	}
-	if(p1.button.options){cx=0;cy=0;cz=0;}
+
+	if(p1.stick.lx>0.2 || p1.stick.ly>0.2 || p1.stick.lx<-0.2 || p1.stick.ly<-0.2 || p1.stick.rx>0.2 || p1.stick.ry>0.2 || p1.stick.rx<-0.2 || p1.stick.ry<-0.2 || p1.button.cross){
+		controlMode=1;
+	}
 	background(0);
-	rotateX(-radians(cx));
+	ambientLight(100);
+
+	//rotate map
+	rotateX(-radians(213.75));
 	rotateY(radians(cy));
-	specularMaterial(0,0,255);
+
+	//lights
+	pointLight(100, 100, 255, -250+px, -250+py, -250+pz);
+
+	//map
 	push();
-	scale(100);
-	translate(0,1);
-	rotateZ(radians(180));
-	pointLight(100, 100, 255, -250, -250, -250);
+	translate(px,py,pz);
+	rotateX(radians(90));
+	normalMaterial(0,255,0);
+  plane(500, 500);
+	pop();
+
+	//teapot
+	push();
+	scale(75);
+	specularMaterial(0,0,255);//teapot material
 	model(teapot);
 	pop();
-	if(p1.stick.rx>0.2 || p1.stick.rx<-0.2 || p1.stick.lx>0.2 || p1.stick.lx<-0.2 || p1.stick.ly>0.2 || p1.stick.ly<-0.2){controlMode=1;}
 }
